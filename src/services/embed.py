cf789257bb705f2f999f06c91e5a2e10ad3f35b9
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-13 20:22:42 +0100

    Store embeddings within custom class not app object

--- src/services/embed.py
+++ src/services/embed.py
@@ -1,14 +1,16 @@
 import tempfile
-from typing import List
-from services.query import SimpleDirectoryReader, VectorStoreIndex
+
+from services.generate_response import SimpleDirectoryReader, VectorStoreIndex
 from services.upload_files import upload_file
 from exceptions.log_handler import system_logger
 from exceptions.errors import FileUploadException
 
+from utils.file_checker import check_file
 from schema import Result
-from utils import check_file
+from state import Embeddings
 
-async def embed_file(files, app):
+
+async def embed_file(files, session_embeddings: Embeddings):
     file_check = check_file(files=files)
     if file_check['status_code'] == 200:
         try:
@@ -16,7 +18,8 @@ async def embed_file(files, app):
                 file_upload = await upload_file(files=files, temp_dir=temp_dir)
                 if file_upload.is_successful:
                     documents = SimpleDirectoryReader(temp_dir).load_data()
-                    app.embeddings = VectorStoreIndex.from_documents(documents=documents) # store the embeddings in session... not suitable for production
+                    embeddings = VectorStoreIndex.from_documents(documents=documents) 
+                    session_embeddings.set_state(embeddings) # store the embeddings in session... not suitable for production
                     return Result(
                         is_successful=True,
                         detail="Embeddings generated successfully.",
