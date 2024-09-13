f21d1c0b38df8707b3c690286b92308f778d682b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-13 10:52:23 +0100

    Configure api endpoints

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from typing import List
 from fastapi import FastAPI, Request, UploadFile
-from chat import *
+from services.embed import embed_file
+
 
 app = FastAPI()
 
@@ -8,20 +9,40 @@ app = FastAPI()
 async def health():
     return {
         "application": "Simple LLM API",
-        "message": "running successfully"
+        "message": "running successfully",
     }
 
 @app.post('/upload')
-async def process(
-    files: List[UploadFile] = None,
-    urls: List[str] = None,
-):
-    
-    if files:
-        pass
-        
+async def process(files: List[UploadFile] = None, urls: List[str] = None):
+    try:
+        res = embed_file(files, app)
+        if res.error_message: 
+            # Use 400 STATUS CODE FOR FILE CHECK ERROR.. 
+            # Perhaps a pointer to wrap all embed related func in the service
+            return {
+                "detail": res.detail,
+                "status_code": 400,
+            }
+        if res.is_successful:    
+            return {
+                "detail": "Embeddings generated successfully.",
+                "status_code": 200,
+            }
+        else:
+            return {
+                "detail": res.detail,
+                "status_code": 500,
+            }  
+    except Exception as e:
+        return {
+            "detail": "Could not generate embeddings.",
+            "status_code": 500,
+        }
+
 
 @app.post('/generate')
 async def generate_chat(request: Request):
     query = await request.json()
     pass
+
+
