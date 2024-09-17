782e258584a3847ab546f376217a0fe7eacd42bf
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-17 19:47:32 +0100

    Configure streamlit UI

---
+++ src/frontend/main.py
@@ -0,0 +1,267 @@
+# Create streamlit app
+
+from pathlib import Path
+from typing import List, Tuple
+import tempfile
+
+import streamlit as st
+import requests
+
+ 
+# configure web page
+st.set_page_config(page_title="RAG.ai", page_icon="✨", layout="wide")
+
+# initialise session state for tracking user input and responses
+if 'responses' not in st.session_state:
+    st.session_state.responses = []
+
+def scroll_to_bottom_of_page():
+    """ Return focus to bottom of page once a question is entered."""
+    # Define the scroll operation as a function and pass in something unique for each
+    # page load that it needs to re-evaluate where "bottom" is
+    js = f"""
+    <script>
+        function scroll(dummy_var_to_force_repeat_execution){{
+            var textAreas = parent.document.querySelectorAll('section.main');
+            for (let index = 0; index < textAreas.length; index++) {{
+                textAreas[index].scrollTop = textAreas[index].scrollHeight;
+            }}
+        }}
+        scroll({len(st.session_state.responses)})
+    </script>
+    """
+
+    js = '''
+    <script>
+        var body = window.parent.document.querySelector(".main");
+        console.log(body);
+        body.scrollTop = 0;
+    </script>
+    '''
+    st.components.v1.html(js)
+
+def display_chat_history():
+    with st.container():
+        for response in st.session_state.responses:
+            left, right = st.columns(2)
+            right.markdown(f"""
+                <div style="background-color:#262730; padding:10px; border-radius:20px;">
+                    <p style="font-family:Arial, sans-serif; font-color: #2f2f2f; ">{response['user']}</p>
+                </div>
+                </br>
+                """, 
+                unsafe_allow_html=True,
+            )
+            st.empty().markdown(f"""
+                <div style="padding:10px; border-radius:5px;">
+                    <p style="font-family:Arial, sans-serif; font-color: #2f2f2f; ">{response['bot']}</p>
+                </div>
+                </br>
+                """, 
+                unsafe_allow_html=True,
+            )
+
+def post_request_to_api(url: str, question: str, model: str, temperature: float):
+    """Send the user input to backend API"""
+    response = requests.post(
+        url,
+        json={
+            "question": question,
+            "model": model,
+            "temperature": temperature,
+        },
+        stream=True,
+    )
+    return response
+
+def write_file_content(file, temp_dir) -> Tuple:
+    file_path = Path(temp_dir, file.name)
+    file_object = file.read()
+    try:    
+        with open(file_path, "wb") as buffer:
+            buffer.write(file_object) 
+            return 1  
+    except Exception as e:
+        return 0
+        
+def read_file_content(temp_dir):
+    paths = Path(temp_dir).glob("**/*")
+    return paths
+
+def configure_file_payload(file: Path) -> Tuple:
+    return ('files', (file.name, file.read_bytes(), 'application/pdf'))
+
+def upload_files_to_api(url: str, files: List):
+    """Upload files to backend API"""
+    with tempfile.TemporaryDirectory() as tempdir:
+        all_files = [write_file_content(file, tempdir) for file in files]
+        print("all files", all_files)
+        all_file_content = [configure_file_payload(file) for file in read_file_content(tempdir)]
+        response = requests.post(url, files=all_file_content) # stream=True,
+        return response
+
+def handle_file_upload(file_input, backend_url): # default selection is Stream
+    if file_input:
+        # add user input to session state
+        st.session_state.responses.append({'user': file_input, 'bot': None})
+
+        # prepare empty container to update the bot's response in real time
+        response_container = st.empty()
+
+        response = upload_files_to_api(url=backend_url, files=file_input)
+        print("fast api", response)
+        res_detail = response.json()
+        print("resp det", res_detail)
+        if response.status_code == 200:
+            st.session_state["file_uploaded"] = False
+            bot_response = res_detail["detail"]
+            # display bot's response with adaptable height
+            st.markdown(
+                f"""
+                <div style="padding:10px; border-radius: 5px;">
+                    <p style="font-family: Arial, sans-serif; font-color: #2f2f2f">{bot_response.strip()}</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+            # update the latest bot response in session state
+            st.session_state.responses[-1]['bot'] = bot_response.strip()
+
+        else:
+            response_container.markdown(
+                f"""<p style='color:red;'>Error: {res_detail}</p>""", 
+                unsafe_allow_html=True,
+            )
+        # clear input box for next question
+        # st.session_state.current_input = ""
+
+def handle_message(user_input, backend_url, selected_model, temperature): # default selection is Stream
+    if user_input:
+        # add user input to session state
+        st.session_state.responses.append({'user': user_input, 'bot': None})
+
+        response = post_request_to_api(
+            url=backend_url,
+            question=user_input,
+            model=selected_model,
+            temperature=temperature,
+        )
+
+        # prepare empty container to update the bot's response in real time
+        response_container = st.empty()
+
+        if response.status_code == 200:
+            bot_response = ""
+            # if selected_response_type == chatbot.output_type[0]:
+            #     # stream response from backend
+            #     for chunk in response.iter_content(chunk_size=None, decode_unicode=True):
+            #         bot_response += chunk
+            #         # update response container with the latest bot response
+            #         response_container.markdown(
+            #             f"""
+            #             <div style="padding:10px; border-radius: 5px;">
+            #                 <p style="font-family: Arial, sans-serif; font-color: #2f2f2f">{bot_response.strip()}</p>
+            #             </div>
+            #             """,
+            #             unsafe_allow_html=True,
+            #         )
+            #         # update the latest bot response in session state
+            #         st.session_state.responses[-1]['bot'] = bot_response.strip()
+
+            # else:
+            # collect the batch response
+            for chunk in response.iter_content(chunk_size=None, decode_unicode=True):
+                bot_response += chunk
+            
+            # display bot's response with adaptable height
+            st.markdown(
+                f"""
+                <div style="padding:10px; border-radius: 5px;">
+                    <p style="font-family: Arial, sans-serif; font-color: #2f2f2f">{bot_response.strip()}</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+            # update the latest bot response in session state
+            st.session_state.responses[-1]['bot'] = bot_response.strip()
+
+        else:
+            response_container.markdown(
+                "<p style='color:red;'>Error: Unable to get a response from the server.</p>", 
+                unsafe_allow_html=True,
+            )
+        # clear input box for next question
+        # st.session_state.current_input = ""
+
+def get_configs():
+    with st.sidebar:
+        with st.form("🗀 File Input", clear_on_submit=True):
+            uploaded_files = st.file_uploader(
+                "Upload local files:",
+                type=["txt", "pdf", "docx", "csv", "jpg", "png", "jpeg"],
+                accept_multiple_files=True,
+            )
+            submitted = st.form_submit_button("Submit")
+            # for file in uploaded_files:
+            #     f
+        if uploaded_files and submitted:
+            st.session_state["uploaded_files"] = uploaded_files
+
+        expander = st.expander("Select your response type", expanded=True)
+        with expander:
+            selected_response_type = st.radio("Output types", ["A", "B"], index=None)
+        # select model and training parameters
+        expander = st.expander("⚒ Temperature Configuration")
+        with expander:
+            temperature = st.slider("Temperature", min_value=0.01, max_value=5.0, value=0.0, step=0.01, format="%.1f")
+        selected_model = st.selectbox("Select your preferred model: ", ["llama-3.1-70b-versatile","llama-3.1-8b-instant","mixtral-8x7b-32768"])
+
+        # set_tokens = st.selectbox("Please select length of output", chatbot.token_class.keys())
+        return selected_model, selected_response_type, temperature, submitted #, set_tokens
+
+def change_file_state():
+    st.session_state["file_uploaded"] = True
+
+
+
+# main layout
+def main():
+    scroll_to_bottom_of_page()
+    display_chat_history()
+
+    # selected_model, selected_response_type, temperature, set_tokens = get_configs()
+    selected_model, selected_response_type, temperature, submitted = get_configs()
+    # selected_response_type = selected_response_type # if selected_response_type else chatbot.output_type[0] # default selection is Stream 
+    
+    if submitted:
+        handle_file_upload(file_input=st.session_state["uploaded_files"], backend_url="http://127.0.0.1:8888/upload")
+
+    # collect user input below the chat history
+    prompt = st.chat_input("Ask a question")
+    if prompt:
+        user_input = prompt
+
+        # define the URL of the backend API
+        # if selected_response_type == chatbot.output_type[0]:
+        #     backend_url = "http://127.0.0.1:5000/chat_stream"
+        # else:
+        backend_url = "http://127.0.0.1:8888/generate"
+        
+        if user_input:
+            left, right = st.columns(2)
+            right.markdown(f"""
+            <div style="background-color:#262730; padding:10px; bottom-margin: 1px; border-radius:20px;">
+                <p style="font-family:Arial, sans-serif; font-color: #2f2f2f; ">{user_input}</p>
+            </div>
+            </br>
+            """, 
+            unsafe_allow_html=True)
+            handle_message(
+                user_input=user_input,
+                backend_url=backend_url,
+                selected_model=selected_model,
+                temperature=temperature,
+            )
+
+if __name__ =="__main__":
+    main()
