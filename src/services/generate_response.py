11b920c358c3a7d9e02478bea094b5f0ba08afbe
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-19 10:42:47 +0100

    refactor: Use embeddings from persistence location

--- src/services/generate_response.py
+++ src/services/generate_response.py
@@ -1,4 +1,11 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings #, ServiceContext
+from pathlib import Path
+from llama_index.core import (
+    VectorStoreIndex, 
+    SimpleDirectoryReader, 
+    Settings, 
+    StorageContext, 
+    load_index_from_storage,
+)
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 from services.select_llm_client import LLMClient
@@ -11,23 +18,32 @@ Settings.embed_model = HuggingFaceEmbedding(
     model_name="BAAI/bge-small-en-v1.5"
 )
 
+def get_embeddings_index():
+    """Return the index of stored embeddings""" 
+    storage_path = Path("vector_db")
+    storage_context = StorageContext.from_defaults(persist_dir=storage_path) 
+    index = load_index_from_storage(storage_context)
+    return index
 
 def query_engine(query: str, vector_store_index: VectorStoreIndex, llm_client):
     query_engine = vector_store_index.as_query_engine(llm=llm_client, similarity_top_k=10)
     response = query_engine.query(query)
     return response
 
-def generate(question: str, embeddings: Embeddings, model: str, temperature: float):
+def generate(question: str, model: str, temperature: float):
     try:
         init_llm_client = LLMClient(temperature=temperature)
         client = init_llm_client.select_client(model)
+        index = get_embeddings_index()
+        print("index obtained successfully")
+        
         response = query_engine( 
-            llm_client=client,
-            vector_store_index=embeddings.get_state(),
             query=question,
+            llm_client=client,
+            vector_store_index=index,
         )
         return response.response
     except Exception as e:
         system_logger.error("An error occurred during response generation.", exc_info=1)
         raise e
-        
+         
