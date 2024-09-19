14208b317db5dc62f8b2797ce51e77f835e728ca
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-19 09:55:56 +0100

    Define function to persist embeddings after creation

--- src/services/generate_embeddings.py
+++ src/services/generate_embeddings.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import tempfile
 
 from services.generate_response import SimpleDirectoryReader, VectorStoreIndex
@@ -20,6 +21,40 @@ async def embed_file(files, session_embeddings: Embeddings):
                     documents = SimpleDirectoryReader(temp_dir).load_data()
                     embeddings = VectorStoreIndex.from_documents(documents=documents) 
                     session_embeddings.set_state(embeddings) # store the embeddings in session... not suitable for production
+                    return Result(
+                        is_successful=True,
+                        detail="Embeddings generated successfully.",
+                    )  
+        except FileUploadException:
+            system_logger.error("An error occurred during file upload.", exc_info=1)
+            return Result(
+                is_successful=False,
+                detail="File Upload Error.",
+            )        
+        except Exception as e:
+            system_logger.error("An error occurred during embedding.", exc_info=1)
+            return Result(
+                is_successful=False,
+                detail="Could not generate embeddings.",
+            )
+    else:
+        return Result(
+            is_successful=False,
+            error_message="File check failed",
+            detail=file_check['detail'],
+    ) 
+
+async def embed_file_and_persist(files):
+    file_check = check_files(files=files)
+    if file_check.get('status_code') == 200:
+        try:
+            vector_storage_path = Path('vector_db')
+            with tempfile.TemporaryDirectory() as temp_dir:
+                file_upload = await upload_file(files=files, temp_dir=temp_dir)
+                if file_upload.is_successful:
+                    documents = SimpleDirectoryReader(temp_dir).load_data()
+                    embeddings = VectorStoreIndex.from_documents(documents=documents) 
+                    embeddings.storage_context.persist(persist_dir=vector_storage_path)
                     return Result(
                         is_successful=True,
                         detail="Embeddings generated successfully.",
