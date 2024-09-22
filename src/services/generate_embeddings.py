6b51d6d984e06970c028ef8025ed2f5089294827
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-22 01:10:05 +0100

    Persist embeddings to in-memory chromaDB instance

--- src/services/generate_embeddings.py
+++ src/services/generate_embeddings.py
@@ -1,28 +1,29 @@
-from pathlib import Path
 import tempfile
 from typing import List
 
-from services.generate_response import SimpleDirectoryReader, VectorStoreIndex
+from services.generate_response import (
+    SimpleDirectoryReader, 
+    VectorStoreIndex, 
+    ChromaVectorStore, 
+    StorageContext,
+)
 from services.upload_files import upload_file
+from services.get_chroma import init_chroma
 from exceptions.log_handler import system_logger
 from exceptions.errors import FileUploadException
 
 from utils.file_validation import check_files
-from utils.config import vector_store_dir
 from schema import Result
 
 
-async def embed_file_and_persist(files: List, project_embeddings_dir: str):
+async def convert_files_to_embeddings(files: List, collection: str) -> Result:
     file_check = check_files(files=files)
     if file_check.get('status_code') == 200:
         try:
-            vector_storage_path = create_storage_path(project_embeddings_dir)
             with tempfile.TemporaryDirectory() as temp_dir:
                 file_upload = await upload_file(files=files, temp_dir=temp_dir)
                 if file_upload.is_successful:
-                    documents = SimpleDirectoryReader(temp_dir).load_data()
-                    embeddings = VectorStoreIndex.from_documents(documents=documents) 
-                    embeddings.storage_context.persist(persist_dir=vector_storage_path)
+                    embed_documents(collection=collection, dir=temp_dir)
                     return Result(
                         is_successful=True,
                         detail="Embeddings generated successfully.",
@@ -46,11 +47,12 @@ async def embed_file_and_persist(files: List, project_embeddings_dir: str):
             detail=file_check['detail'],
     ) 
 
-def create_storage_path(embeddings_dir: str, parent_dir: str = vector_store_dir):
-    try:
-        Path(parent_dir).mkdir(exist_ok=True)
-        storage_path = Path(parent_dir, embeddings_dir)
-        storage_path.mkdir(exist_ok=True)
-        return storage_path
-    except Exception as e:
-        raise e
+def embed_documents(collection: str, dir: str) -> VectorStoreIndex:
+    documents = SimpleDirectoryReader(dir).load_data()
+    col = init_chroma(collection_name=collection)
+    vector_store = ChromaVectorStore(chroma_collection=col)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    embeddings = VectorStoreIndex.from_documents(documents=documents, storage_context=storage_context) 
+    return embeddings
+
+    
