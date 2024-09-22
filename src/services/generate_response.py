742c012518e78c67989f214aa06e552d54679344
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-22 14:53:49 +0100

    Add function to get top_k, reorder functions

--- src/services/generate_response.py
+++ src/services/generate_response.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from llama_index.core import (
     Settings, 
     VectorStoreIndex, 
@@ -7,8 +8,8 @@ from llama_index.core import (
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
 
-from services.select_llm_client import LLMClient
-from services.get_chroma import init_chroma
+from services.select_llm_client import LLMService
+from services.get_chroma import init_chroma, get_knowledge_base_size
 from exceptions.log_handler import system_logger
 
 print("Finished llama imports...") 
@@ -17,36 +18,51 @@ Settings.embed_model = HuggingFaceEmbedding(
     model_name="BAAI/bge-small-en-v1.5"
 ) 
 
-def retrieve_embeddings_index(collection: str) -> VectorStoreIndex:
-    """Return the index of embeddings stored in chroma""" 
-    chroma_collection = init_chroma(collection_name=collection)
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection, collection_name=collection)
-    index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
-    return index
-
-def query_engine(query: str, vector_store_index: VectorStoreIndex, llm_client: LLMClient):
-    query_engine = vector_store_index.as_query_engine(llm=llm_client, similarity_top_k=10)
-    response = query_engine.query(query)
-    return response
-
-def generate(
-    question: str, 
-    model: str, 
-    temperature: float, 
-    max_tokens: int, 
-    collection: str,
-):
+def generate(question: str, model: str, temperature: float, max_tokens: int, collection: str):
     try:
-        init_llm_client = LLMClient(max_output_tokens=max_tokens, temperature=temperature)
-        client = init_llm_client.select_client(model)
-        index = retrieve_embeddings_index(collection)        
+        llm = LLMService(max_output_tokens=max_tokens, temperature=temperature)
+        client = llm.select_client(model)
+        index, col_size = retrieve_embeddings_index_and_size(collection) 
+        top_k = get_choice_k(col_size)       
         response = query_engine( 
             query=question,
             llm_client=client,
             vector_store_index=index,
+            choice_k=top_k,
         )
         return response.response
     except Exception as e:
         system_logger.error("An error occurred during response generation.", exc_info=1)
         raise e
+
+def retrieve_embeddings_index_and_size(collection: str) -> Tuple[VectorStoreIndex, int]:
+    """Return a tuple with the index of embeddings stored in chroma and their size""" 
+    chroma_collection = init_chroma(collection_name=collection)
+    retrieved_col_size = get_knowledge_base_size(collection=chroma_collection)
+    print(f"Retrieved collection size: {retrieved_col_size}")
+    # TODO: Set up embeddings logger to track embedding sizes
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection, collection_name=collection)
+    index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+    return index, retrieved_col_size
+
+def get_choice_k(collection_size: int) -> int:
+    try:
+        int(collection_size)
+    except ValueError as e:
+        raise e
+    
+    if collection_size > 150:
+        return 40
+    elif collection_size > 50:
+        return 15
+    elif collection_size > 10:
+        return 5
+    else:
+        return collection_size
+
+def query_engine(query: str, vector_store_index: VectorStoreIndex, llm_client: LLMService, choice_k: int):
+    query_engine = vector_store_index.as_query_engine(llm=llm_client, similarity_top_k=choice_k)
+    response = query_engine.query(query)
+    return response
+
          
