a27029b66dfda2342b2bf70cab78247b6d892d86
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-24 12:14:55 +0100

    Include file upload

--- src/services/retrieval.py
+++ src/services/retrieval.py
@@ -1,5 +1,9 @@
-import tempfile
+from pathlib import Path
 from typing import List
+import tempfile
+
+from fastapi import UploadFile
+from werkzeug.utils import secure_filename
 
 from services.generation import (
     SimpleDirectoryReader, 
@@ -7,23 +11,22 @@ from services.generation import (
     ChromaVectorStore, 
     StorageContext,
 )
-from services.upload_files import upload_file
 from services.chroma_db import init_chroma, get_knowledge_base_size
+from utils.file_validation import check_files
+from schema import Result
 from exceptions.log_handler import system_logger
 from exceptions.errors import FileUploadException
 
-from utils.file_validation import check_files
-from schema import Result
-
-
-async def convert_files_to_embeddings(files: List, collection: str) -> Result:
+    
+    
+async def process_files_for_embeddings(files: List, collection: str) -> Result:
     file_check = check_files(files=files)
     if file_check.get('status_code') == 200:
         try:
             with tempfile.TemporaryDirectory() as temp_dir:
                 file_upload = await upload_file(files=files, temp_dir=temp_dir)
                 if file_upload.is_successful:
-                    embed_documents(collection=collection, dir=temp_dir)
+                    create_embeddings(collection=collection, dir=temp_dir)
                     return Result(
                         is_successful=True,
                         detail="Embeddings generated successfully.",
@@ -47,7 +50,27 @@ async def convert_files_to_embeddings(files: List, collection: str) -> Result:
             detail=file_check['detail'],
     ) 
 
-def embed_documents(collection: str, dir: str) -> VectorStoreIndex:
+async def upload_file(files: List[UploadFile], temp_dir: str) -> Result:
+    try:
+        for file in files:
+            filename = secure_filename(file.filename)
+            file_path = Path(temp_dir, filename)
+            file_object = await file.read()
+            with open(file_path, "wb") as buffer:
+                buffer.write(file_object)
+        return Result(
+            is_successful=True,
+            detail="Files uploaded successfully.",
+        )
+    except Exception as e:
+        message = f"An error occurred during upload."
+        system_logger.error(
+            message,
+            exc_info=1
+        )
+        raise FileUploadException(message)
+
+def create_embeddings(collection: str, dir: str) -> VectorStoreIndex:
     documents = SimpleDirectoryReader(dir).load_data()
     col = init_chroma(collection_name=collection)
     col_size_before_embedding = get_knowledge_base_size(collection=col)
