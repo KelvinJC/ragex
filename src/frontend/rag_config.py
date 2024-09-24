f175740430a4ea3527369deed578d77fcb267014
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-09-24 13:19:00 +0100

    Edit configs

--- src/frontend/rag_config.py
+++ src/frontend/rag_config.py
@@ -3,7 +3,6 @@
 import streamlit as st
 
 selected_model = None
-selected_response_type = None
 temperature = None
 submitted = None
 max_tokens = None
@@ -22,24 +21,26 @@ def get_configs():
         if uploaded_files and submitted:
             st.session_state["uploaded_files"] = uploaded_files
 
+        ex = st.expander("⚒ Conversation Configuration", expanded=True)
+        with ex:
+            last_submitted_embeddings = (len(st.session_state["projects"]) - 1) if st.session_state["projects"] else None
+            selected_project = st.selectbox(
+                "Select embeddings for query: ", 
+                st.session_state["projects"], 
+                on_change=change_id, 
+                index=last_submitted_embeddings,
+            )
         # select model and training parameters
         expander = st.expander("⚒ RAG Pipeline Configuration", expanded=True)
         with expander:
             st.subheader("Adjust RAG Parameters")
-            selected_response_type = st.radio("Output types", ["Stream", "Batch"], index=None)
             max_tokens = st.number_input("Max Tokens", min_value=1, max_value=1024, value=512)
             temperature = st.slider("Temperature", min_value=0.01, max_value=2.0, value=0.0, step=0.01, format="%.1f")
+        
         selected_model = st.selectbox("Select your preferred model: ", ["llama-3.1-70b-versatile","llama-3.1-8b-instant","mixtral-8x7b-32768"])
 
 
-        last_submitted_embeddings = (len(st.session_state["projects"]) - 1) if st.session_state["projects"] else None
-        selected_project = st.selectbox(
-            "Select embeddings for query: ", 
-            st.session_state["projects"], 
-            on_change=change_id, 
-            index=last_submitted_embeddings,
-        )
-        vars = (selected_model, selected_response_type, temperature, submitted, max_tokens, selected_project)
+        vars = (selected_model, temperature, submitted, max_tokens, selected_project)
         return vars 
 
 def change_id():
